9c512d9c4f43a359df581e219e7dd93b5e810ebf
valstrl <valentine.santarelli@epfl.ch>
2017-11-29 13:32:11 +0100

    line chart

--- src/correlation.js
+++ src/correlation.js
@@ -17,31 +17,29 @@ var g = svg.append("g");
 d3.csv("data/output.csv", function(data) {
 });*/
 
-var ctx = document.getElementById("myChart");
+var ctx = document.getElementById("tempratureChart");
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        /*labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],*/
         datasets: [{
-            label: '# of Votes',
+            label: ' °C',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255,99,132,1)'
             ],
-            borderWidth: 1
+            borderWidth: 1,
+            pointBackgroundColor:[
+                'rgba(255,99,132,1)'
+            ],
+            pointBorderColor:[
+                'rgba(255,99,132,1)'
+            ],
+            pointBorderWidth:1,
+            pointRadius:2
         }]
     },
     options: {
