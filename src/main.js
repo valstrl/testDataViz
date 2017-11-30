25bcb071ca391de12b42906742841c35a665e158
valstrl <valentine.santarelli@epfl.ch>
2017-11-30 23:22:43 +0100

    add 3rd slider to show current time displayed

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 
-/*import { makeLineChart } from './graph.js';*/
 //recover data
 var temperature = [12, 19, 3, 5, 2, 3];
 var co2 = [10, 5, 3, 3, 8, 9];
 var months= ['January', 'February', 'March', 'April', 'May', 'June'];
 
-d3.select("body")
-          .append("div")
-            .attr("id","slider");
 var slider = document.getElementById('slider');
 
 //slider
 noUiSlider.create(slider, {
- start: [0, months.length -1],
- connect: true,
+ start: [0, Math.round(months.length/2), months.length -1],
+ connect: [false,true, true,false],
  step:1,
  range: {
    'min': 0,
@@ -29,11 +25,9 @@ noUiSlider.create(slider, {
 
 //function to add string label to the pips
 function changePipLabel(idSlider,months){
-  for (i = 0; i < months.length -1 ; i++) {
+  for (i = 0; i < months.length  ; i++) {
     change_label= vsprintf('#%s > div.noUi-pips.noUi-pips-horizontal > div:nth-child(%d)', [idSlider, 2*i + 2]);
-
     $(change_label).text(months[i]);
-
 }
 };
 changePipLabel('slider', months);
@@ -43,16 +37,15 @@ var months_selected;
 slider.noUiSlider.on('update', function( values, handle ) {
 
 	 months_id_selected= slider.noUiSlider.get();;
-   months_id_selected[1]= Number(months_id_selected[1]) + 1; // recover id of months selected
-
-
+   months_id_selected[1]= Number(months_id_selected[1]) ;
+   months_id_selected[2]= Number(months_id_selected[2]) + 1; // recover id of months selected
 
    //create graph for temperatureChart
    color_temperature='rgba(255,99,132,1)';
    label_temperature= ' °C';
    idLineChart_temperature="temperatureChart";
    //canvas for temperature chart
-   d3.select("body")
+   d3.select("#panel")
              .append("canvas")
                .attr("id",idLineChart_temperature);
    makeLineChart(temperature, months, months_id_selected, color_temperature, label_temperature, idLineChart_temperature)
@@ -61,7 +54,7 @@ slider.noUiSlider.on('update', function( values, handle ) {
    color_co2='rgba(99,255,132,1)';
    label_co2= ' kt';
    idLineChart_co2="CO2Chart";
-   d3.select("body")
+   d3.select("#panel")
              .append("canvas")
                .attr("id",idLineChart_co2);
    makeLineChart(co2, months, months_id_selected, color_co2, label_co2, idLineChart_co2)
@@ -70,10 +63,10 @@ slider.noUiSlider.on('update', function( values, handle ) {
 
 function makeLineChart(dataset, months, months_id_selected, color, label, idLineChart){
 var data = {
-    labels: months.slice(months_id_selected[0],months_id_selected[1]),
+    labels: months.slice(months_id_selected[0],months_id_selected[2]),
     datasets: [{
         label: label,
-        data: dataset.slice(months_id_selected[0],months_id_selected[1]),
+        data: dataset.slice(months_id_selected[0],months_id_selected[2]),
         borderColor: color,
         fill: false,
         borderWidth: 1,
@@ -81,14 +74,28 @@ var data = {
         pointBorderColor:color,
         pointBorderWidth:1,
         pointRadius:2
-    }]
+    },
+    {
+
+        data: [{
+          x:months[months_id_selected[1]],
+          y: dataset[months_id_selected[1]]
+        }],
+        pointBackgroundColor:'rgba(255,0,0,1)',
+        pointBorderColor:'rgba(255,0,0,1)',
+        pointBorderWidth:2,
+        pointRadius:4,
+        type: 'scatter',
+
+
+  }]
 };
 
 var option = {
     scales: {
       xAxes: [{
             type: 'category',
-            labels: months.slice(months_id_selected[0],months_id_selected[1])
+            labels: months.slice(months_id_selected[0],months_id_selected[2])
         }],
         yAxes: [{
             ticks: {
