ba2b6cf2ceac504a7a6340419113564ff56b47a5
valstrl <valentine.santarelli@epfl.ch>
2017-11-30 11:06:26 +0100

    slider + 2 charts

--- src/correlation.js
+++ src/correlation.js
@@ -16,80 +16,221 @@ var g = svg.append("g");
 
 d3.csv("data/output.csv", function(data) {
 });*/
-var data_test = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-    datasets: [{
-        label: ' °C',
-        data: [12, 19, 3, 5, 2, 3] ,
-        borderColor: [
-            'rgba(255,99,132,1)'
-        ],
-        fill: false,
-        borderWidth: 1,
-        pointBackgroundColor:[
-            'rgba(255,99,132,1)',
-            'rgba(255,99,132,1)',
-            'rgba(255,99,132,1)',
-            'rgba(255,99,132,1)',
-            'rgba(255,99,132,1)'
-        ],
-        pointBorderColor:[
-            'rgba(255,99,132,1)',
-            'rgba(255,99,132,1)',
-            'rgba(255,99,132,1)',
-            'rgba(255,99,132,1)',
-            'rgba(255,99,132,1)'
-        ],
-        pointBorderWidth:1,
-        pointRadius:2
-    }]
-};
 
-var option_test = {
-    scales: {
-      xAxes: [{
-            type: 'category',
-            labels: ['January', 'February', 'March', 'April', 'May', 'June']
-        }],
-        yAxes: [{
-            ticks: {
-                beginAtZero:true
-            }
-        }]
-    }
-};
+var temp = [12, 19, 3, 5, 2, 3];
+var co2 = [10, 5, 3, 3, 8, 9];
+var months= ['January', 'February', 'March', 'April', 'May', 'June'];
+var slider = document.getElementById('slider');
 
-var ctx = document.getElementById("temperatureChart");
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: data_test,
-    options: option_test
+noUiSlider.create(slider, {
+ start: [0, months.length -1],
+ connect: true,
+ step:1,
+ range: {
+   'min': 0,
+   'max':months.length -1,
+ }
 });
 
- var ctx2 = document.getElementById("temperatureChart2").getContext('2d');
- var slider = document.getElementById('slider');
+   var data_test = {
+       labels: months,
+       datasets: [{
+           label: ' °C',
+           data: temp,
+           borderColor: [
+               'rgba(255,99,132,1)'
+           ],
+           fill: false,
+           borderWidth: 1,
+           pointBackgroundColor:[
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)'
+           ],
+           pointBorderColor:[
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)'
+           ],
+           pointBorderWidth:1,
+           pointRadius:2
+       }]
+   };
 
-rs = new RangeSliderChart({
+   var data_test2 = {
+       labels: months,
+       datasets: [{
+           label: 'kt',
+           data: co2,
+           borderColor: [
+               'rgba(99,255,132,1)'
+           ],
+           fill: false,
+           borderWidth: 1,
+           pointBackgroundColor:[
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)'
+           ],
+           pointBorderColor:[
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)'
+           ],
+           pointBorderWidth:1,
+           pointRadius:2
+       }]
+   };
+
+   var option_test = {
+       scales: {
+         xAxes: [{
+               type: 'category',
+               labels: months
+           }],
+           yAxes: [{
+               ticks: {
+                   beginAtZero:true
+               }
+           }]
+       }
+   };
+
+   var ctx = document.getElementById("temperatureChart");
+   var myChart = new Chart(ctx, {
+       type: 'line',
+       data: data_test,
+       options: option_test
+   });
+
+   var ctx2 = document.getElementById("CO2Chart");
+   var myChart = new Chart(ctx2, {
+       type: 'line',
+       data: data_test2,
+       options: option_test
+   });
+
+var months_selected;
+slider.noUiSlider.on('update', function( values, handle ) {
+	 months_id_selected= slider.noUiSlider.get();;
+   /*console.log(months_id_selected);*/
+
+   var data_test = {
+       labels: months.slice(months_id_selected[0],months_id_selected[1]),
+       datasets: [{
+           label: ' °C',
+           data: temp.slice(months_id_selected[0],months_id_selected[1]),
+           borderColor: [
+               'rgba(255,99,132,1)'
+           ],
+           fill: false,
+           borderWidth: 1,
+           pointBackgroundColor:[
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)'
+           ],
+           pointBorderColor:[
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)'
+           ],
+           pointBorderWidth:1,
+           pointRadius:2
+       }]
+   };
+
+   var data_test2 = {
+       labels: months.slice(months_id_selected[0],months_id_selected[1]),
+       datasets: [{
+           label: 'kt',
+           data: co2.slice(months_id_selected[0],months_id_selected[1]),
+           borderColor: [
+               'rgba(99,255,132,1)'
+           ],
+           fill: false,
+           borderWidth: 1,
+           pointBackgroundColor:[
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)'
+           ],
+           pointBorderColor:[
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)',
+               'rgba(99,255,132,1)'
+           ],
+           pointBorderWidth:1,
+           pointRadius:2
+       }]
+   };
+
+   var option_test = {
+       scales: {
+         xAxes: [{
+               type: 'category',
+               labels: months.slice(months_id_selected[0],months_id_selected[1])
+           }],
+           yAxes: [{
+               ticks: {
+                   beginAtZero:true
+               }
+           }]
+       }
+   };
+
+   var ctx = document.getElementById("temperatureChart");
+   var myChart = new Chart(ctx, {
+       type: 'line',
+       data: data_test,
+       options: option_test
+   });
+
+   var ctx2 = document.getElementById("CO2Chart");
+   var myChart = new Chart(ctx2, {
+       type: 'line',
+       data: data_test2,
+       options: option_test
+   });
+
+});
+
+
+
+
+
+
+
+
+ /*var ctx2 = document.getElementById("temperatureChart2").getContext('2d');*/
+/*rs = new RangeSliderChart({
 
   chartData: data_test,
   chartOpts: option_test,
   chartType: 'line',
   chartCTX: ctx2,
-
-  /*chartElement: slider,*/
-  initial:[1,6]
-});
-
-/*
-
-noUiSlider.create(slider, {
-	start: [20, 80],
-	connect: true,
-	range: {
-		'min': 0,
-		'max': 100
-	}
+  initial:[0,5]
 });*/
+
+
+
+
 /* d3.select('#slider').call(d3.slider().axis(true).value( [ 10, 25 ] ).on("slide", function(evt, value) {
   d3.select('#slidertextmin').text(value[ 0 ]);
   d3.select('#slidertextmax').text(value[ 1 ]);
